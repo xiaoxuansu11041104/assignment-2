c6abb607fe0d573eefd2956f811db393f302f1ee
xiaoxuansu11041104 <xiaoxuan1104@gmail.com>
2024-10-11 13:11:58 -0700

    Created new folders for components and screens

---
+++ Navigation.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+
+export default function Navigation() {
+  return (
+    <View>
+      <Text>Navigation</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
