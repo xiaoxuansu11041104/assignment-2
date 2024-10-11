8f210414d277897c9d3da4e5b25d36b76eb83bf9
xiaoxuansu11041104 <xiaoxuan1104@gmail.com>
2024-10-11 13:24:49 -0700

    Define the stacks for activity and diet

--- Navigation.js
+++ Navigation.js
@@ -4,6 +4,40 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 
+import Activities from './Screens/ActivitiesScreen';
+import AddActivity from './Screens/AddActivityScreen';
+import Diet from './Screens/DietScreen';
+import Settings from './Screens/SettingsScreen';
+import AddDiet from './Screens/AddDietScreen';
+
+// Importing Ionicons for using icons in the bottom tab navigator
+import { Ionicons } from '@expo/vector-icons'; 
+
+// Creating instances of Bottom Tab Navigator and Native Stack Navigator
+const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+
+// Defining the stack for Activities, including the screen to add an activity
+function ActivitiesStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Activities" component={Activities} />
+      <Stack.Screen name="AddActivity" component={AddActivity} />
+    </Stack.Navigator>
+  );
+}
+
+// Defining the stack for Diet, including the screen to add a diet entry
+function DietStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Diet" component={Diet} />
+      <Stack.Screen name="AddDiet" component={AddDiet} />
+    </Stack.Navigator>
+  );
+}
+
+
 export default function Navigation() {
   return (
     <View>
